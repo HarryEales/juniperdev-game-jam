d041998dec97e0ab237ffd755bca8e4bab17b11b
HarryEales <emeraldkrew3000@gmail.com>
2026-06-24 22:11:08 +1000

    no more f1 driver dog
    made the dogs movement more normal looking. no more weird u-turns or completely unpredictable movement

--- actors/base_dog/states/base_dog_regular_state.gd
+++ actors/base_dog/states/base_dog_regular_state.gd
@@ -15,13 +15,21 @@ var _movement_handler : MovementHandler
 
 var _random_direction_array : Array[Vector2] = [
 	Vector2(1, 0),
+	Vector2(1, 0.5),
 	Vector2(1, 1),
+	Vector2(0.5, 1),
 	Vector2(0, 1),
+	Vector2(-0.5, 1),
 	Vector2(-1, 1),
+	Vector2(-1, 0.5),
 	Vector2(-1, 0),
+	Vector2(-1, -0.5),
 	Vector2(-1, -1),
+	Vector2(-0.5, -1),
 	Vector2(0, -1),
-	Vector2(1, -1)
+	Vector2(0.5, -1),
+	Vector2(1, -1),
+	Vector2(1, -0.5)
 ]
 
 var _current_random_direction : int = 0
@@ -38,7 +46,7 @@ func _enter() -> void:
 	_movement_handler = _root_entity._handler_parent._handler_dict["MovementHandler"]
 	
 	_current_action = CURRENT_ACTION.WALK
-	_current_random_direction = randi_range(0, 7)
+	_current_random_direction = randi_range(0, _random_direction_array.size() - 1)
 	_movement_timer.wait_time = randf_range(min_max_walk_time.x, min_max_walk_time.y)
 	_current_walk_interval = 1
 	_current_total_walk_interval = randi_range(min_max_walk_intervals.x, min_max_walk_intervals.y)
@@ -49,7 +57,6 @@ func _enter() -> void:
 func _update(delta : float) -> void:
 	if _current_action == CURRENT_ACTION.WALK:
 		_movement_handler.move(delta, _random_direction_array[_current_random_direction])
-		#print(_random_direction_array[_current_random_direction])
 	else:
 		_movement_handler.move(delta, Vector2.ZERO)
 
@@ -60,9 +67,19 @@ func timer_done() -> void:
 		_movement_timer.wait_time = randf_range(min_max_idle_time.x, min_max_idle_time.y)
 		_movement_timer.start()
 	elif _current_action == CURRENT_ACTION.WALK and _current_walk_interval < _current_total_walk_interval:
-		_current_random_direction = randi_range(0, _random_direction_array.size() - 1)
+		var positive_or_negative : int = randi_range(1, 2)
+		if positive_or_negative == 1:
+			_current_random_direction -= 1
+			if _current_random_direction < 0:
+				_current_random_direction = _random_direction_array.size() - 1
+		else:
+			_current_random_direction += 1
+			if _current_random_direction >= _random_direction_array.size():
+				_current_random_direction = 0
+		
 		_movement_timer.wait_time = randf_range(min_max_walk_time.x, min_max_walk_time.y)
 		_current_walk_interval += 1
+		print(_random_direction_array[_current_random_direction])
 		_movement_timer.start()
 	else:
 		_current_action = CURRENT_ACTION.WALK
@@ -70,4 +87,5 @@ func timer_done() -> void:
 		_movement_timer.wait_time = randf_range(min_max_walk_time.x, min_max_walk_time.y)
 		_current_walk_interval = 1
 		_current_total_walk_interval = randi_range(min_max_walk_intervals.x, min_max_walk_intervals.y)
+		print(_random_direction_array[_current_random_direction])
 		_movement_timer.start()
